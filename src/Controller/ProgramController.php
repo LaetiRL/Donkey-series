6e3f5982de2503b63fe291ec0c7b00f343210bcd
Laetitia Rodriguez <rodriguezl.laetitia@gmail.com>
2022-04-07 15:41:26 +0200

    challenge8

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 use App\Repository\ProgramRepository;
+use App\Repository\SeasonRepository;
 
 /**
  * @Route("/program", name="program_")
@@ -26,7 +27,7 @@ class ProgramController extends AbstractController
     }
 
     /**
-     * @Route("/show/{id<\d+>}", methods={"GET"}, name="show")
+     * @Route("/{id<\d+>}", methods={"GET"}, name="show")
      */
     public function show(ProgramRepository $programs, int $id): Response
     {
@@ -34,12 +35,41 @@ class ProgramController extends AbstractController
         
         if (!$program) {
             throw $this->createNotFoundException(
-                'No program with id '.$id.' found in program\'s table.'
+                'No program with id '.$id.' found.'
+            );
+        }
+        
+        $seasons = $program->getSeasons();
+  
+        return $this->render(
+            'program/show.html.twig',
+            ['id' => $program, 'seasons' => $seasons]
+        );
+    }
+
+    /**
+     * @Route("/{programId<\d+>}/season/{seasonId<\d+>}", methods={"GET"}, name="season_show")
+     */
+    public function showSeason(ProgramRepository $programs, SeasonRepository $seasons, int $programId, int $seasonId): Response
+    {   
+        $program = $programs->findOneBy(['id' => $programId]);
+        $season = $seasons->findOneBy(['id' => $seasonId]);
+        $episodes = $season->getEpisodes();
+    
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id '.$programId.' found.'
+            );
+        }
+        if (!$season) {
+            throw $this->createNotFoundException(
+                'No season with id '.$seasonId.' found.'
             );
         }
         return $this->render(
-            'program/show.html.twig',
-            ['id' => $program]
+            'program/season_show.html.twig',
+            ['program' => $program, 'season' => $season, 'episodes' => $episodes]
         );
+
     }
 }
