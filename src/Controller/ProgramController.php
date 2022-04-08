5c4ae4b36b22b5bc7884ba5a133aefa350a719fc
Laetitia Rodriguez <rodriguezl.laetitia@gmail.com>
2022-04-08 11:50:50 +0200

    challenge10

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -5,11 +5,17 @@ namespace App\Controller;
 use App\Entity\Program;
 use App\Entity\Season;
 use App\Entity\Episode;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+use Doctrine\Persistence\ManagerRegistry;
 
 use App\Repository\ProgramRepository;
+
+use App\Form\ProgramType;
 
 /**
  * @Route("/program", name="program_")
@@ -26,6 +32,33 @@ class ProgramController extends AbstractController
             'program/index.html.twig',
             ['programs' => $programs->findAll()]
         );
+    }
+
+    /**
+     * @Route("/new", name="new")
+     */
+    public function new(Request $request, ManagerRegistry $doctrine): Response
+    {
+        // Create a new Category Object
+        $program = new Program();
+        // Create the associated Form
+        $form = $this->createForm(ProgramType::class, $program);
+        // Get data from HTTP request
+        $form->handleRequest($request);
+        // Was the form submitted ?
+        if ($form->isSubmitted()) {
+            // Deal with the submitted data
+            // Get the Entity Manager
+            $entityManager = $doctrine->getManager();
+            // Persist Category Object
+            $entityManager->persist($program);
+            // Flush the persisted object
+            $entityManager->flush();
+            // Finally redirect to categories list
+            return $this->redirectToRoute('program_index');
+        }
+        // Render the form
+        return $this->render('program/new.html.twig', ["form" => $form->createView()]);
     }
 
     /**
