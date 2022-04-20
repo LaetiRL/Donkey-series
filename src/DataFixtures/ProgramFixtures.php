75938c4130fba0a9ff56d286e6f5f3ff6efff000
Laetitia Rodriguez <rodriguezl.laetitia@gmail.com>
2022-04-20 10:52:12 +0200

    challenge15

--- src/DataFixtures/ProgramFixtures.php
+++ src/DataFixtures/ProgramFixtures.php
@@ -17,7 +17,9 @@ class ProgramFixtures extends Fixture implements DependentFixtureInterface
         $program->setSummary("L'histoire suit le personnage de Rick Grimes, adjoint du shérif. Il se réveille d'un coma de plusieurs semaines pour découvrir que la population a été ravagée par une épidémie inconnue qui transforme les êtres humains en morts-vivants. Avec un groupe de rescapé, ils sont amenés à devoir survivre dans un monde post-apocalyptique.");
         $program->setPoster("https://m.media-amazon.com/images/I/71loVl64+NL._AC_SL1000_.jpg");
         $program->setCategory($this->getReference('category_4'));
-        $program->getActors($this->getReference(ActorFixtures::ACTORS[6]));
+        $program->addActor($this->getReference(ActorFixtures::ACTORS[6]));
+        $program->addActor($this->getReference(ActorFixtures::ACTORS[7]));
+        $program->addActor($this->getReference(ActorFixtures::ACTORS[8]));
         
 
         $manager->persist($program);
